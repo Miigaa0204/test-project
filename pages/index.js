60704fb99488f13418247f20d4c5201a84b6ac9a
Miigaa0204 <mytawochir@gmail.com>
2024-09-11 18:59:21 +0800

    done

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,233 @@
-import Image from "next/image";
-import localFont from "next/font/local";
+import { Card } from "@/components/ui/card";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+export default function home() {
+  let friends = [
+    {
+      id: 1,
+      age: 23,
+      sex: "male",
+      name: "Naraa",
+      skin: "yellow",
+      profession: "dev",
+    },
+    {
+      id: 2,
+      age: 22,
+      sex: "female",
+      name: "Maralaa",
+      skin: "white",
+      profession: "teacher",
+    },
+    {
+      id: 3,
+      age: 35,
+      sex: "male",
+      name: "Nyambaa",
+      skin: "brown",
+      profession: "businessman",
+    },
+    {
+      id: 4,
+      age: 17,
+      sex: "male",
+      name: "Tsenguun",
+      skin: "brown",
+      profession: "student",
+    },
+    {
+      id: 5,
+      age: 21,
+      sex: "male",
+      name: "Jerry",
+      skin: "brown",
+      profession: "dev",
+    },
+    {
+      id: 6,
+      age: 22,
+      sex: "male",
+      name: "Temuujin",
+      skin: "white",
+      profession: "dev",
+    },
+    {
+      id: 7,
+      age: 21,
+      sex: "male",
+      name: "Miigaa",
+      skin: "brown",
+      profession: "dev",
+    },
+    {
+      id: 8,
+      age: 21,
+      sex: "male",
+      name: "Hangal",
+      skin: "white",
+      profession: "dev",
+    },
+    {
+      id: 9,
+      age: 21,
+      sex: "male",
+      name: "Zoloo",
+      skin: "brown",
+      profession: "dev",
+    },
+    {
+      id: 10,
+      age: 21,
+      sex: "male",
+      name: "Badral",
+      skin: "brown",
+      profession: "dev",
+    },
+  ];
 
-export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className="m-auto text-center">
+      <div className="grid grid-cols-3 gap-6 p-10 ">
+        <Card className="p-10 text-start">
+          Name:{friends[0].name}
+          <br />
+          Age:{friends[0].age}
+          <br />
+          Sex:{friends[0].sex}
+          <br />
+          Profession:{friends[0].profession}
+          <br />
+          Skin:{friends[0].skin}
+          <br />
+          Id:{friends[0].id}
+          <br />
+        </Card>
+        <Card className="p-10 text-start">
+          Name:{friends[1].name}
+          <br />
+          Age:{friends[1].age}
+          <br />
+          Sex:{friends[1].sex}
+          <br />
+          Profession:{friends[1].profession}
+          <br />
+          Skin:{friends[1].skin}
+          <br />
+          Id:{friends[1].id}
+          <br />
+        </Card>
+        <Card className="p-10 text-start">
+          Name:{friends[2].name}
+          <br />
+          Age:{friends[2].age}
+          <br />
+          Sex:{friends[2].sex}
+          <br />
+          Profession:{friends[2].profession}
+          <br />
+          Skin:{friends[2].skin}
+          <br />
+          Id:{friends[2].id}
+          <br />
+        </Card>
+        <Card className="p-10 text-start">
+          Name:{friends[3].name}
+          <br />
+          Age:{friends[3].age}
+          <br />
+          Sex:{friends[3].sex}
+          <br />
+          Profession:{friends[3].profession}
+          <br />
+          Skin:{friends[3].skin}
+          <br />
+          Id:{friends[3].id}
+          <br />
+        </Card>
+        <Card className="p-10 text-start">
+          Name:{friends[4].name}
+          <br />
+          Age:{friends[4].age}
+          <br />
+          Sex:{friends[4].sex}
+          <br />
+          Profession:{friends[4].profession}
+          <br />
+          Skin:{friends[4].skin}
+          <br />
+          Id:{friends[4].id}
+          <br />
+        </Card>
+        <Card className="p-10 text-start">
+          Name:{friends[5].name}
+          <br />
+          Age:{friends[5].age}
+          <br />
+          Sex:{friends[5].sex}
+          <br />
+          Profession:{friends[5].profession}
+          <br />
+          Skin:{friends[5].skin}
+          <br />
+          Id:{friends[5].id}
+          <br />
+        </Card>
+        <Card className="p-10 text-start">
+          Name:{friends[6].name}
+          <br />
+          Age:{friends[6].age}
+          <br />
+          Sex:{friends[6].sex}
+          <br />
+          Profession:{friends[6].profession}
+          <br />
+          Skin:{friends[6].skin}
+          <br />
+          Id:{friends[6].id}
+          <br />
+        </Card>
+        <Card className="p-10 text-start">
+          Name:{friends[7].name}
+          <br />
+          Age:{friends[7].age}
+          <br />
+          Sex:{friends[7].sex}
+          <br />
+          Profession:{friends[7].profession}
+          <br />
+          Skin:{friends[7].skin}
+          <br />
+          Id:{friends[7].id}
+          <br />
+        </Card>
+        <Card className="p-10 text-start">
+          Name:{friends[8].name}
+          <br />
+          Age:{friends[8].age}
+          <br />
+          Sex:{friends[8].sex}
+          <br />
+          Profession:{friends[8].profession}
+          <br />
+          Skin:{friends[8].skin}
+          <br />
+          Id:{friends[8].id}
+          <br />
+        </Card>
+        <Card className="p-10 text-start">
+          Name:{friends[9].name}
+          <br />
+          Age:{friends[9].age}
+          <br />
+          Sex:{friends[9].sex}
+          <br />
+          Profession:{friends[9].profession}
+          <br />
+          Skin:{friends[9].skin}
+          <br />
+          Id:{friends[9].id}
+          <br />
+        </Card>
+      </div>
     </div>
   );
 }
