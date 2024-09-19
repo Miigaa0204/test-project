68db945598d678bd12909e8ae465e375cd045ab6
Miigaa0204 <mytawochir@gmial.com>
2024-09-19 14:27:26 +0800

    update

--- components/Test.jsx
+++ components/Test.jsx
@@ -13,13 +13,40 @@ const Test = () => {
     visible ? "" : "";
   }
 
+  // {visible ? "visible ni true baina" : "visible ni false baina"}
+
+  const [user, setUser] = useState(false);
+ 
+
   return (
-    <div>
-      <Button onClick={() => setVisible(!visible)}>change state</Button>
-      <div>{visible ? "visible ni true baina" : "visible ni false baina"}</div>
-      {/* <div className="flex items-center">CONTENT</div> */}
+    <div className="flex gap-5">
+      <div className="flex flex-col gap-5 ">
+        <Button
+          className="text-green-500 bg-transparent border-none"
+          onClick={() => setUser(true )}
+        >
+          User section
+        </Button>
+        <Button
+          onClick={() => setUser(false)}
+          className="text-green-500 bg-transparent"
+        >
+          Order history
+        </Button>
+      </div>
+      <div className="flex">
+        <div>{user ?  test() : "orderhistory"}</div>
+      </div>
     </div>
   );
 };
+
+const test = ()=> {
+  return (
+    <div>
+      user
+    </div>
+  )
+}
 
 export default Test;
